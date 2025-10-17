f8e2928ca4fd011d3bb84cd6f1bb32dbb4c8442f
Yuk1 <23020110@vnu.edu.vn>
2025-10-18 01:36:16 +0700

    community update 2

--- src/main/java/com/volunteerhub/community/service/UserProfileService.java
+++ src/main/java/com/volunteerhub/community/service/UserProfileService.java
@@ -1,6 +1,7 @@
 package com.volunteerhub.community.service;
 
 import com.volunteerhub.community.dto.input.EditEventInput;
+import com.volunteerhub.community.dto.input.EditUserProfileInput;
 import com.volunteerhub.community.dto.output.UserProfileDto;
 
 public interface UserProfileService {
