4bc58e5eab0bb24af7cdb1a428e32fa63a019c3d
MinhD2 <160483338+d2mteam@users.noreply.github.com>
2025-12-17 23:47:32 +0700

    Add event volunteer export endpoint

---
+++ src/main/java/com/volunteerhub/community/dto/rest/request/EventVolunteerExportRequest.java
@@ -0,0 +1,30 @@
+package com.volunteerhub.community.dto.rest.request;
+
+import com.volunteerhub.community.model.db_enum.EventRole;
+import com.volunteerhub.community.model.db_enum.EventState;
+import com.volunteerhub.community.model.db_enum.ParticipationStatus;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+public class EventVolunteerExportRequest {
+    @NotEmpty(message = "At least one field is required")
+    @Builder.Default
+    private List<String> fields = new ArrayList<>();
+
+    @Builder.Default
+    private String format = "json";
+
+    private List<Long> eventIds;
+
+    private List<EventRole> eventRoles;
+
+    private List<ParticipationStatus> participationStatuses;
+
+    private List<EventState> eventStates;
+}
